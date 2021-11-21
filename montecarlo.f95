580f67994aea0115cfb52b2c079a8c5806cc1a32
LuckyToast <91315326+LuckyToaster@users.noreply.github.com>
2021-11-21 21:07:14 +0100

    Add files via upload

---
+++ montecarlo.f95
@@ -0,0 +1,61 @@
+program montecarlo_method
+    implicit none
+    ! circle area = pi r**2
+    ! sqr area = 4**2
+    ! p (in circle) = A circle / A sqr = pi 2^2 / 4^2 = pi / 4
+    ! 4 basic types: integer, logical, real, character
+
+    ! assuming center of circle is p(0,0) ==> so x = rand(-2,2) and y = rand(-2,2)
+    ! (Xc,Yc) (Xp, Yp) 
+    ! point (Xp, Yp) is inside circle if d < r & on the circle if d = r
+    ! save time by d^2 < r^2 for inside circle & d^2 = r^2 for on circle
+    ! d^2= (Xp - Xc)^2 + (Yp - Yc) (and compare with r^2)
+
+    integer :: k, i, inside_circle, outside_circle
+    real, parameter :: pi = 4.*atan(1.)
+    real :: r, area_circle, area_sqr
+    real :: x_c, y_c, x_p, y_p
+    ! random number stuff
+    real :: low, high, random
+    integer, dimension(8) :: values 
+    integer, dimension(:), allocatable :: seed
+    r = 2.0
+    area_circle = pi*r**2
+    area_sqr = r**2
+    x_c = 0
+    y_c = 0
+    low = -2.0
+    high = 2.0
+    inside_circle = 0
+    outside_circle = 0
+
+    ! random number generation weidness
+    call date_and_time(VALUES=values)
+    call random_seed(size=k)
+    allocate(seed(k))
+    seed(:) = values(:)
+    call random_seed(put=seed)
+
+    do  i = 0,10000
+        call random_number(random)
+        x_p = low + (high - low)* random
+        call random_number(random)
+        y_p = low + (high - low) * random
+
+        print*, '(x_p, y_p) = ', x_p, y_p
+        if ((x_p**2 + y_p**2) .le. r**2) then
+            inside_circle = inside_circle + 1
+            print*, "point inside of circle"
+        else 
+            print*, "point is outside of circle"     
+            outside_circle = outside_circle + 1
+        endif
+    end do
+
+    print*, ' pi: ', pi
+    print*, ' area square = ', area_sqr
+    print*, ' area circle = ', area_circle
+    print*, "Number of points inside circle: ", inside_circle
+    print*, "Number of points outside circle: ", outside_circle
+
+end program montecarlo_method
